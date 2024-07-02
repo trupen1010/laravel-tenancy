04301c143f7d2ff027d39ccf0ee1321e8efe8744
TrupenAdroja <121307095+trupen1010@users.noreply.github.com>
2024-07-02 17:34:25 +0530

    Update update.blade.php

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Tenant</title>
+    <title>Edit Tenant this is test</title>
     <style>
         body {
             font-family: Arial, sans-serif;
